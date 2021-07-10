52c1725640c956a3308eba04101b77f77e7e55a7
Takahiro <tkhr_621@docomo.ne.jp>
2021-07-11 04:06:16 +0900

    del

--- pages/dogs/[id].tsx
+++ pages/dogs/[id].tsx
@@ -2,7 +2,6 @@ import { useRouter } from 'next/router'
 
 const Post = () => {
     const router = useRouter()
-    console.log(router.query)
     const {id} = router.query
     return (
         <div>
